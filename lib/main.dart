65be69287103929cef5f78a1be96ba756fa3ac23
ELFAIZE-Youssef <m.youssef.elfaize@gmail.com>
2025-11-30 13:28:10 +0100

    Add Firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:smart_app_elfaize/screens/home_page.dart';
 import 'package:smart_app_elfaize/screens/login_page.dart';
 import 'package:smart_app_elfaize/screens/register_page.dart';
@@ -7,7 +9,11 @@ import 'screens/home_page.dart';
 import 'screens/login_page.dart';
 import 'screens/register_page.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
